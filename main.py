b6c639c3a7828e387d2395327211691fa11ad66b
Jesper Lundberg <Jeppsson@fam-lundberg.se>
2022-05-11 15:07:32 +0200

    Implemented color for the letters

--- main.py
+++ main.py
@@ -2,22 +2,24 @@
 #TEINF20
 #Wordle a fun game - main
 
-from resources import Wordle
+from lib2to3.pytree import convert
+from typing import List
+from resources import LetterCondition, Wordle
+from colorama import Fore
 
 def main():
     print("Hello Wordle!")
     wordle = Wordle("APPLE")
 
     while wordle.can_attempt:
-        x = input("Skriv in din gissning: ")
+        x = input("\nSkriv in din gissning: ")
 
         if len(x) != wordle.Word_length:
-            print(f"Ordet måste vara {wordle.Word_length} bokstäver långt! Testa igen.")
+            print(Fore.RED + f"Ordet måste vara {wordle.Word_length} bokstäver långt! Testa igen." + Fore.RESET)
             continue
 
         wordle.attempt(x)
-        result = wordle.guess(x)
-        print(*result, sep="\n")
+        display_results(wordle)
 
     if wordle.correct_guess:
             print("Du gissade rätt!")
@@ -25,9 +27,47 @@ def main():
         print("Du klarade tyvärr inte att lösa ut det ord som söktes.")
     
 
+def display_results(wordle: Wordle):
+    print("\nDina resultat hittills...\n")
+    print(f"Du har {wordle.attempts_left} försök kvar.\n")
+    
+    lines = []
 
+    for word in wordle.attempts:
+        result = wordle.guess(word)
+        colored_result_str = convert_result_to_color(result)
+        lines.append(colored_result_str)
+    
+    for _ in range(wordle.attempts_left):
+        #print("_ " * wordle.Word_length)
+        lines.append(" ".join(["_"] * wordle.Word_length))
 
+    border_around_wordle(lines)
 
+def convert_result_to_color(result: List[LetterCondition]):
+    result_w_color = []
+    for letter in result:
+        if letter.in_position:
+            color = Fore.GREEN
+        elif letter.in_word:
+            color = Fore.YELLOW
+        else:
+            color = Fore.RED
+        colored_letter = color + letter.character + Fore.RESET
+        result_w_color.append(colored_letter)
+    return " ".join(result_w_color)
+
+def border_around_wordle(lines : List[str], size: int=9, pad : int=1):
+    content_length = size + pad * 2
+    top_border = "┌" + "─" * content_length + "┐"
+    bottom_border = "└" + "─" * content_length + "┘"
+    space = " " * pad
+    print(top_border)
+
+    for line in lines:
+        print("│" + space + line + space + "│")
+        
+    print(bottom_border)
 
 if __name__ == "__main__":
     main()
